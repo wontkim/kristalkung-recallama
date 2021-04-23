30c74aa8947edc0f22d2f998a49e9c5ea2c572da
Kristal <kristalkung@gmail.com>
2021-04-23 04:16:52 +0000

    created server.py

---
+++ server.py
@@ -0,0 +1,24 @@
+"""Server for movie ratings app."""
+
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db
+import crud
+import model
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "kksecretkey"
+app.jinja_env.undefined = StrictUndefined
+
+
+# app routes go here
+
+
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
+
