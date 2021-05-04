8f5c7b3eb71616e69f24c94e2e04afd0ba9c8b42
Kristal Kung <kristalkung@gmail.com>
2021-05-04 19:27:35 +0000

    made basic react pages for login.jsx and signup.jsx

---
+++ static/js/login.jsx
@@ -0,0 +1,29 @@
+function Login() {
+    return(
+        <React.Fragment>
+            <h2>
+                Login to your Recallama account
+            </h2>
+            <form>
+                <label>
+                    Email: 
+                    <input type="text" email="email" />
+                </label>
+                <br />
+                <label>
+                    Password: 
+                    <input type="text" password="password" />
+                </label>
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+            <br />
+            <br />
+            <a href="/signup">
+                Don't have an account? Click here to create one.
+            </a>
+        </React.Fragment>
+    )
+}
+
+ReactDOM.render(<Login />, document.getElementById('app'));
---
+++ static/js/signup.jsx
@@ -0,0 +1,39 @@
+function SignUp() {
+    return(
+        <React.Fragment>
+            <h2>
+                Sign up for a Recallama account
+            </h2>
+            <form>
+            <label>
+                    First Name: 
+                    <input type="text" fname="fname" />
+                </label>
+                <br />
+                <label>
+                    Last Name: 
+                    <input type="text" lname="lname" />
+                </label>
+                <br />
+                <label>
+                    Email: 
+                    <input type="text" email="email" />
+                </label>
+                <br />
+                <label>
+                    Password: 
+                    <input type="text" password="password" />
+                </label>
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+            <br />
+            <br />
+            <a href="/login">
+                Already have an account? Click here to login.
+            </a>
+        </React.Fragment>
+    )
+}
+
+ReactDOM.render(<SignUp />, document.getElementById('app'));
