826b1fcca9178792d92ac50eab2eb09a43a175c1
Kristal Kung <kristalkung@gmail.com>
2021-05-08 22:33:41 +0000

    cleaned up login.jsx

--- static/js/login.jsx
+++ static/js/login.jsx
@@ -2,43 +2,8 @@
 
 function LogIn() {
 
-  let history = useHistory();
-
 	const [email, setEmail] = React.useState("");
 	const [password, setPassword] = React.useState("");
-
-	// function handleLogin(evt) {
-	// 	evt.preventDefault();
-		
-	// 	// make a data option and put your email and password in it
-	// 	const data = {
-	// 		email: email,
-	// 		password: password
-	// 	}
-
-	// 	const options = {
-	// 		'method': 'POST',
-	// 		headers: {
-	// 			'Content-Type': 'application/json' 
-	// 			// tells the server during the post that this is a json string
-	// 		},
-	// 		// turn data into JSON
-	// 		body: JSON.stringify(data)
-	// 	}
-		
-	// 	fetch('/api/login', options)
-	// 	.then(response => response.json())
-	// 	.then(data => {
-	// 		if (data == "login successful") {
-  //       console.log("login successful")
-  //       history.push('/search')
-	// 			// redirects back to homepage
-	// 		} else {
-	// 			console.log("login failed")
-	// 		}
-	// 	})
-
-	// }
 
 	function handleEmailChange(evt) {
 		setEmail(evt.target.value)
